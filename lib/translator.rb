713aa6687fb3d3f222fbc7a4220cdfe3883765c4
Codey Pinero <codey.pinero@gmail.com>
2019-03-29 02:40:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,6 +14,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
   result = library["get_meaning"][emoticon]
+  result ? result : "Sorry that emoticon was not found"
 end
 
 def get_english_meaning
