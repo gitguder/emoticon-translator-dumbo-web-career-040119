38c82ec004bb3143a9a7ee6fd98fb039658eadaa
Codey Pinero <codey.pinero@gmail.com>
2019-03-29 02:34:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,7 @@ def load_library(file_path)
   library = { "get_meaning" => {}, "get_emoticon" => {} }
   YAML.load_file(file_path).each do |meaning, array|
     english, japanese = array
+    library["get_emoticon"][english] = japanese
 end
 
 def get_japanese_emoticon
